7aa0efb1b5982f23c0a7dc5b76ddc18adb99e148
Konstantinos Despoinidis <kdesp2003@gmail.com>
2024-06-19 19:53:09 +0300

    updated docs

--- docs/webc-server_8h.js
+++ docs/webc-server_8h.js
@@ -1,5 +1,6 @@
 var webc_server_8h =
 [
     [ "ServeExported", "webc-server_8h.html#ae14f13b68a40dafa7f67c0adeb435eed", null ],
+    [ "ServeExportedRoot", "webc-server_8h.html#a7d208fb4788fb6736e7269882d209120", null ],
     [ "ServeTree", "webc-server_8h.html#aa0115deee883605eba16337e8c5ab030", null ]
 ];
